49ac1c7e4d3bbc2dc08bb903f40d75e200cb2e17
uandhgroup <36882850+uandhgroup@users.noreply.github.com>
2018-12-12 22:59:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ version = '0.0.1'
 setup(
     name='internal_aireldentalchairs_com',
     version=version,
-    description='ERPNext.com website',
+    description='AirelDentalChairs.com website',
     author='Frappe',
-    author_email='info@erpnext.com',
+    author_email='web@uandh.in',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
